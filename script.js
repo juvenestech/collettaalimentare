2ba4a79ab7c58fd29afd6a2b9a60cdcb7eb88e2c
BisUmTo <bisumto@gmail.com>
2025-11-26 18:17:10 +0100

    Changed ClientID

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
   initializeCalendar();
 
   const GOOGLE_CLIENT_ID =
-    "203366866884-2v8pvrqc6h5n6krlje9729dbnf539f7k.apps.googleusercontent.com";
+    "529145784780-9oltgi13lg1u7srjp8empl2boe9q4b19.apps.googleusercontent.com";
   const GOOGLE_SCOPES = "openid email profile";
   const GOOGLE_AUTH_ENDPOINT = "https://accounts.google.com/o/oauth2/v2/auth";
   const GOOGLE_REDIRECT_URI =
